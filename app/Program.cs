84393720117d2f1f11047e50c6744a26fe61bbc4
Jöran Malek <alivedevil@users.noreply.github.com>
2020-10-30 09:17:07 +0100

    Init
    https://github.com/microsoft/CsWinRT/issues/549 repro

---
+++ app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lib_test.Class c = new lib_test.Class();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lib-test/Class.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::lib_test::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::lib_test::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
